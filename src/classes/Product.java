f2c107e1942dc0a57c01be7b6906e9bff9476e11
tarek-sabony-dev <tareksabony@gmail.com>
2025-12-10 13:15:28 +0300

    fc

--- src/classes/Product.java
+++ src/classes/Product.java
@@ -5,5 +5,5 @@ import java.util.HashMap;
 public class Product {
     public int id;
     public String name;
-    public HashMap<Item, Integer> requiredItems = new HashMap<>();
+    public HashMap<Item, Integer> resquiredItems = new HashMap<>();
 }
